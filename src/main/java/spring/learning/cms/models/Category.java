16accc18ce566f4a33358aff9b224f55ec8ed31e
张生虎 <zhangshenghu@zhangshenghudeMacBook-Pro.local>
2018-04-05 22:14:18 +0800

    first app using spring boot

--- src/main/java/spring/learning/cms/models/Category.java
+++ src/main/java/spring/learning/cms/models/Category.java
@@ -1,20 +1,10 @@
 package spring.learning.cms.models;
 
 import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Data
-@Entity
-@Table(name="category")
 public class Category {
-    @Id
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
     String id;
     String name;
 }
